7b86796c3d5a897200da2cba40d4b7ed0839f9f0
Thanos Polychronakis <thanpolas@gmail.com>
2021-08-24 22:27:58 +0300

    introduce event bus and local event types

--- app/entities/events/events.ent.js
+++ app/entities/events/events.ent.js
@@ -2,8 +2,15 @@
  * @fileoverview Main Event bus (if needed) and event related constants.
  */
 
+const EventEmitter = require('events');
+
+const { eventTypes } = require('./constants/event-types.const');
 const { LogEvents } = require('./constants/log-events.const');
 
 const entity = (module.exports = {});
 
+/** @type {events?} Eventemitter instance. */
+entity.events = new EventEmitter({ captureRejections: true });
+
 entity.LogEvents = LogEvents;
+entity.eventTypes = eventTypes;
