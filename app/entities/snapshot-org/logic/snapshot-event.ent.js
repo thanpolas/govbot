09714c00a28ebbad0cb79080403033e1c741d22b
Thanos Polychronakis <thanpolas@gmail.com>
2021-08-24 22:36:23 +0300

    webhook handler of snapshot

---
+++ app/entities/snapshot-org/logic/snapshot-event.ent.js
@@ -0,0 +1,50 @@
+/**
+ * @fileoverview Handles webhook events from snapshot.org.
+ */
+
+const { events, eventTypes } = require('../../events');
+
+const log = require('../../../services/log.service').get();
+
+const {
+  SNAPSHOT_PROPOSAL_CREATED,
+  SNAPSHOT_PROPOSAL_START,
+  SNAPSHOT_PROPOSAL_END,
+  SNAPSHOT_PROPOSAL_DELETED,
+} = eventTypes;
+
+/** @const {string} EventMap Mapping of snapshot event types to local event types */
+const EventMap = {
+  'proposal/created': SNAPSHOT_PROPOSAL_CREATED,
+  'proposal/start': SNAPSHOT_PROPOSAL_START,
+  'proposal/end': SNAPSHOT_PROPOSAL_END,
+  'proposal/deleted ': SNAPSHOT_PROPOSAL_DELETED,
+};
+
+const entity = (module.exports = {});
+
+/**
+ * Handles the webhook event, will dispatch an event.
+ *
+ * @param {Object} data The webhook payload.
+ * @return {Promise<void>} A Promise.
+ */
+entity.handleWebhook = async (data) => {
+  // {
+  //   id: 'proposal/QmZ21uS8tVucpaNq2LZCbZUmHhYYXunC1ZS2gPDNWwPWD9',
+  //   event: 'proposal/created',
+  //   space: 'yam.eth',
+  //   expire: 1620947058
+  // }
+
+  const { id: proposalId, event: eventType, space } = data;
+
+  await log.info(
+    `Webhook event. Type: "${eventType}" Space: "${space}" id: ${proposalId}`,
+  );
+
+  const proposal = await entity.fetchProposal(proposalId);
+
+  const localEventType = EventMap[eventType];
+  events.emit(localEventType, proposal);
+};
