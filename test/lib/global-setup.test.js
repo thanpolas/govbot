f91f667d751f149c7c24d7bbc795dbc4402b546c
Thanos Polychronakis <thanpolas@gmail.com>
2021-08-24 16:05:35 +0300

    tweak global-setup script

--- test/lib/global-setup.test.js
+++ test/lib/global-setup.test.js
@@ -3,10 +3,11 @@
  */
 
 const databaseLib = require('./database.lib');
+const { database_name } = require('../../package.json');
 
 module.exports = async () => {
   console.log('\n\nGlobal Test Setup initiates...');
   if (process.env.NUKE_TEST_DB) {
-    await databaseLib.recreateDatabase();
+    await databaseLib.recreateDatabase(`${database_name}-test`);
   }
 };
