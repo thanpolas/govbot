08f86b34ab8db876e4f0adaefa0a85a02fd6e235
Thanos Polychronakis <thanpolas@gmail.com>
2021-08-25 13:36:34 +0300

    create the snapshot fixture

--- test/fixtures/snapshot.fix.js
+++ test/fixtures/snapshot.fix.js
@@ -5,59 +5,40 @@
 
 const fix = (module.exports = {});
 
-fix.divergenceStandard = () => {
+fix.proposalId = 'QmQbcxLpGENeDauCAsh3BXy9H9fiiK46JEfnLqG3s8iMbN';
+
+fix.webhookCreate = () => {
   return {
-    state: {
-      heartbeat: 3,
-      blockNumber: 1054363,
-      feedPrices: {
-        BTCUSD: 45894.6,
-        ETHUSD: 3154.3,
-        LINKUSD: 24.51,
-        UNIUSD: 28.39,
-        AAVEUSD: 381.7,
-      },
-      oraclePrices: {
-        AAVEUSD: 380.36,
-        BTCUSD: 45976.8,
-        ETHUSD: 3158.7,
-        LINKUSD: 24.53,
-        UNIUSD: 28.4,
-      },
-      synthPrices: {
-        BTCUSD: 45976.8,
-        ETHUSD: 3158.7,
-        LINKUSD: 24.53,
-        UNIUSD: 28.4,
-        AAVEUSD: 380.36,
-      },
-    },
-    oracleToFeed: {
-      BTCUSD: 0.001791277886444842,
-      ETHUSD: 0.001417108654507615,
-      LINKUSD: 0.0008122396478202898,
-      UNIUSD: 0.0005470742095599057,
-      AAVEUSD: -0.0035201366487180863,
-    },
+    id: `proposal/${fix.proposalId}`,
+    event: 'proposal/created',
+    space: 'uniswap',
+    expire: 1620947058,
   };
 };
 
-fix.divergenceOneOpportunity = () => {
-  const divergenceOneOpportunity = fix.divergenceStandard();
-  divergenceOneOpportunity.state.heartbeat = 5;
-  divergenceOneOpportunity.state.blockNumber = 1054365;
-  divergenceOneOpportunity.state.feedPrices.BTCUSD = 47356.1;
-  divergenceOneOpportunity.oracleToFeed.BTCUSD = 0.03;
-
-  return divergenceOneOpportunity;
+fix.webhookStart = () => {
+  return {
+    id: `proposal/${fix.proposalId}`,
+    event: 'proposal/start',
+    space: 'uniswap',
+    expire: 1620947058,
+  };
 };
 
-fix.divergenceTwoOpportunities = () => {
-  const divergenceTwoOpportunities = fix.divergenceOneOpportunity();
-  divergenceTwoOpportunities.state.heartbeat = 8;
-  divergenceTwoOpportunities.state.blockNumber = 1054367;
-  divergenceTwoOpportunities.state.feedPrices.LINKUSD = 25.26;
-  divergenceTwoOpportunities.oracleToFeed.LINKUSD = 0.03;
+fix.webhookEnd = () => {
+  return {
+    id: `proposal/${fix.proposalId}`,
+    event: 'proposal/end',
+    space: 'uniswap',
+    expire: 1620947058,
+  };
+};
 
-  return divergenceTwoOpportunities;
+fix.webhookDeleted = () => {
+  return {
+    id: `proposal/${fix.proposalId}`,
+    event: 'proposal/deleted',
+    space: 'uniswap',
+    expire: 1620947058,
+  };
 };
