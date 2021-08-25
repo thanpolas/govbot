526288c2ded032e51a345dbfd1250c4a756c4e70
Thanos Polychronakis <thanpolas@gmail.com>
2021-08-25 14:14:13 +0300

    write tests for all webhook events

--- test/app/snapshot-webhook.test.js
+++ test/app/snapshot-webhook.test.js
@@ -4,7 +4,12 @@
 
 const testLib = require('../lib/test.lib');
 
-const { webhookCreate } = require('../fixtures/snapshot.fix');
+const {
+  webhookCreate,
+  webhookStart,
+  webhookEnd,
+  webhookDeleted,
+} = require('../fixtures/snapshot.fix');
 const { tweetResponse } = require('../fixtures/twitter.fix');
 const tweet = require('../../app/entities/twitter/logic/send-tweet.ent');
 
@@ -27,6 +32,48 @@ describe('Snapshot Webhooks', () => {
 
       const expectedMessage =
         '🆕 Proposal "Temp Check: Larger Grant Construct // CEA + No Negative Net UNI" CREATED on Snapshot https://snapshot.org/#/uniswap/proposal/QmQbcxLpGENeDauCAsh3BXy9H9fiiK46JEfnLqG3s8iMbN';
+      expect(tweet.sendTweet).toHaveBeenCalledWith(expectedMessage);
+    });
+
+    test('Will handle a start webhook', async () => {
+      const agent = testLib.getAgent();
+
+      const res = await agent.post('/snapshot-webhook').send(webhookStart());
+
+      expect(res.status).toBe(200);
+
+      expect(tweet.sendTweet).toHaveBeenCalledTimes(1);
+
+      const expectedMessage =
+        '📢 Proposal "Temp Check: Larger Grant Construct // CEA + No Negative Net UNI" ACTIVE on Snapshot https://snapshot.org/#/uniswap/proposal/QmQbcxLpGENeDauCAsh3BXy9H9fiiK46JEfnLqG3s8iMbN';
+      expect(tweet.sendTweet).toHaveBeenCalledWith(expectedMessage);
+    });
+
+    test('Will handle an end webhook', async () => {
+      const agent = testLib.getAgent();
+
+      const res = await agent.post('/snapshot-webhook').send(webhookEnd());
+
+      expect(res.status).toBe(200);
+
+      expect(tweet.sendTweet).toHaveBeenCalledTimes(1);
+
+      const expectedMessage =
+        '⛔ Proposal "Temp Check: Larger Grant Construct // CEA + No Negative Net UNI" ENDED on Snapshot https://snapshot.org/#/uniswap/proposal/QmQbcxLpGENeDauCAsh3BXy9H9fiiK46JEfnLqG3s8iMbN';
+      expect(tweet.sendTweet).toHaveBeenCalledWith(expectedMessage);
+    });
+
+    test('Will handle a delete webhook', async () => {
+      const agent = testLib.getAgent();
+
+      const res = await agent.post('/snapshot-webhook').send(webhookDeleted());
+
+      expect(res.status).toBe(200);
+
+      expect(tweet.sendTweet).toHaveBeenCalledTimes(1);
+
+      const expectedMessage =
+        '❌ Proposal "Temp Check: Larger Grant Construct // CEA + No Negative Net UNI" DELETED on Snapshot';
       expect(tweet.sendTweet).toHaveBeenCalledWith(expectedMessage);
     });
   });
