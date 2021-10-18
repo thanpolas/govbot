828456430a799d3402132dabe32b089151dc8e2e
Thanos Polychronakis <thanpolas@gmail.com>
2021-10-18 16:28:44 +0300

    create first migration script with vote_ends_alert table

--- migrations/migrations/20210226111833_first-migration.js
+++ migrations/migrations/20210226111833_first-migration.js
@@ -1,8 +1,20 @@
-// const { defaultFields } = require('../migration-helpers');
+const { defaultFields } = require('../migration-helpers');
 
 exports.up = async function (knex) {
   // Required if gen_random_uuid() PG method doesn't work:
   await knex.schema.raw('CREATE EXTENSION IF NOT EXISTS pgcrypto');
+
+  await knex.schema.createTable('vote_ends_alert', function (table) {
+    table.uuid('id').primary().defaultTo(knex.raw('gen_random_uuid()'));
+
+    table.string('space').notNullable();
+    table.timestamp('expires_at').notNullable();
+    table.timestamp('alert_at').notNullable();
+    table.boolean('alert_twitter_dispatched').notNullable.defaultTo(false);
+    table.boolean('alert_discord_dispatched').notNullable.defaultTo(false);
+
+    defaultFields(table, knex);
+  });
 };
 
 exports.down = async function () {
