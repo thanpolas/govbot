a772e8c447bd04ea72c4c01302bcb612f2c89d25
Thanos Polychronakis <thanpolas@gmail.com>
2021-10-18 16:44:43 +0300

    scaffold vote alert entity

---
+++ app/entities/vote-alert/vote-alert.ent.js
@@ -0,0 +1,12 @@
+/**
+ * @fileoverview Vote alerts business logic.
+ */
+
+const entity = (module.exports = {});
+
+/**
+ * Initialize the twitter service and handlers.
+ *
+ * @return {Promise<void>} A Promise.
+ */
+entity.init = async () => {};
