0350d3fdd8e96c481eca103c68164e739737ae2c
Thanos Polychronakis <thanpolas@gmail.com>
2021-10-19 12:48:50 +0300

    expose and wire voting alert

--- app/entities/vote-alert/vote-alert.ent.js
+++ app/entities/vote-alert/vote-alert.ent.js
@@ -2,11 +2,18 @@
  * @fileoverview Vote alerts business logic.
  */
 
+const { init } = require('./logic/handle-create-vote.ent');
+const { checkForAlert } = require('./logic/check-for-alert.ent');
+
 const entity = (module.exports = {});
+
+entity.checkForAlert = checkForAlert;
 
 /**
  * Initialize the twitter service and handlers.
  *
  * @return {Promise<void>} A Promise.
  */
-entity.init = async () => {};
+entity.init = async () => {
+  await init();
+};
