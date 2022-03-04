9182f975402dfdc1e02091de6ce0cd5c8bf9e825
Thanos Polychronakis <thanpolas@gmail.com>
2022-03-04 10:57:10 +0200

    implement ethgovalerts core

--- app/entities/ethgovalerts/ethgovalerts.ent.js
+++ app/entities/ethgovalerts/ethgovalerts.ent.js
@@ -3,29 +3,6 @@
  *    twitter account as a service.
  */
 
-const config = require('config');
+const { init } = require('./logic/ethgovalerts-core.ent');
 
-const { getEthGovAlertSpaces } = require('./logic/ethgovalerts-spaces.ent');
-const { events, eventTypes } = require('../events');
-
-const {
-  SNAPSHOT_PROPOSAL_START,
-  SNAPSHOT_PROPOSAL_END,
-  PROPOSAL_ENDS_IN_ONE_HOUR,
-} = eventTypes;
-
-exports.getEthGovAlertSpaces = getEthGovAlertSpaces;
-
-/**
- * Initialize the ethgovalerts functionality if enabled.
- */
-exports.init = async () => {
-  if (!config.app.has_ethgovalerts) {
-    return;
-  }
-
-  events.on(
-    SNAPSHOT_PROPOSAL_START,
-    exports._handleEvent.bind(null, configuration, SNAPSHOT_PROPOSAL_START),
-  );
-};
+exports.init = init;
